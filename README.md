3d26772002daca50760bba568d7056ce1a864b17
yashipawar <yashashri2811@gmail.com>
2023-11-24 17:59:09 +0530

    first-commit

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # React Fitness Application
 
-![React Fitness Application](https://i.ibb.co/Yt9spGc/image.png)
+![React Fitness Application] -- Click Here
 
-### [🌟 Become a top 1% Next.js 13 developer in only one course](https://jsmastery.pro/next13)
-### [🚀 Land your dream programming job in 6 months](https://jsmastery.pro/masterclass)
 
-## Launch your development career with project-based coaching - https://www.jsmastery.pro
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import { Box, Typography } from '@mui/material';
 
 const Footer = () => (
-  <Box mt="80px" bgcolor="#FFF3F4">
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
-    </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography>
+  <Box bgcolor="#FFF3F4">
+    <Typography sx={{ fontSize: { lg: '18px', xs: '10px' } }} mt="21px" textAlign="center" pt="20px" pb="20px">Made with ❤️ @YashashriPawar</Typography>
   </Box>
 );
 
