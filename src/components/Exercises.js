19156cdf591ecdc6c4b0cb8930b4d96f95794582
yashipawar <yashashri2811@gmail.com>
2023-12-15 18:58:16 +0530

    solved API issue

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -17,7 +17,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       if (bodyPart === 'all') {
         exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
       } else {
-        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}?limit=10`, exerciseOptions);
       }
 
       setExercises(exercisesData);
